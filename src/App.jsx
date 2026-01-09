f5d492b7250fe68f8e2659dc5f51d51187d52841
Lawrence terrence <lawbabyy@Lawrences-MacBook-Pro.local>
2026-01-09 10:03:41 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello world Lawrence Terrence
         </p>
       </div>
       <p className="read-the-docs">
